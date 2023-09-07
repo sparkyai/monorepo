46d313b60030b2724026e3748039d1b7c50f040c
vladyslav <vladstsk@gmail.com>
2023-09-08 00:57:01 +0400

    fix(tool): correct spacing & auto scroll

--- apps/tool/app/templates/[id]/page.tsx
+++ apps/tool/app/templates/[id]/page.tsx
@@ -1,13 +1,31 @@
 "use client";
 
+import type { UIEvent } from "react";
+import { useEffect, useRef, useState } from "react";
 import { usePreview } from "./preview";
 
 export default function Template() {
+  const ref = useRef<HTMLDivElement>(null);
+  const [top, setTop] = useState(0);
+  const [lock, setLock] = useState(true);
   const { content } = usePreview();
 
+  useEffect(() => {
+    lock && ref.current && ref.current.scrollTo({ top: ref.current.scrollHeight });
+  }, [lock, content]);
+
   return (
-    <div className="flex flex-col gap-3 overflow-y-auto p-4">
-      {content && <p className="rounded-xl border border-blue-500 bg-stone-800 p-4">{content}</p>}
+    <div className="flex flex-col gap-3 overflow-y-auto p-4" onScroll={onScroll} ref={ref}>
+      {content && (
+        <p className="whitespace-break-spaces rounded-xl border border-blue-500 bg-stone-800 p-4">{content}</p>
+      )}
     </div>
   );
+
+  function onScroll(event: UIEvent<HTMLDivElement>) {
+    const { scrollTop, scrollHeight, offsetHeight } = event.currentTarget;
+
+    setLock(scrollTop - top > 0 && scrollTop + offsetHeight === scrollHeight);
+    setTop(scrollTop);
+  }
 }
