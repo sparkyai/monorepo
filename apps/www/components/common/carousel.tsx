7ab99cd128f3838da058b022e6ee3ee21d646abc
vladyslav <vladstsk@gmail.com>
2023-09-18 18:43:21 +0400

    fix(www): carousel overflow effect

--- apps/www/components/common/carousel.tsx
+++ apps/www/components/common/carousel.tsx
@@ -12,7 +12,8 @@ type CarouselProps = {
 
 export default function Carousel(props: CarouselProps) {
   return (
-    <div className="container overflow-x-hidden">
+    <div className="container relative overflow-x-hidden">
+      <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-14 bg-gradient-to-r from-gray-900 from-50%" />
       <Swiper
         autoplay={{
           delay: 3000,
@@ -40,6 +41,7 @@ export default function Carousel(props: CarouselProps) {
           <SwiperSlide className="!flex !h-auto select-none pb-12">{child}</SwiperSlide>
         ))}
       </Swiper>
+      <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-14 bg-gradient-to-l from-gray-900 from-50%" />
     </div>
   );
 }
