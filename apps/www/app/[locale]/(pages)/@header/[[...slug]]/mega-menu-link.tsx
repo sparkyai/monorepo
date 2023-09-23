1e123bdd08c3c16a21f1a3df48c648f5cdde25c0
vladyslav <vladstsk@gmail.com>
2023-09-23 16:16:12 +0400

    feat(www): add mega menu prototype

---
+++ apps/www/app/[locale]/(pages)/@header/[[...slug]]/mega-menu-link.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { twMerge } from "tailwind-merge";
+import NextLink from "next/link";
+import { usePathname } from "next/navigation";
+
+type MegaMenuLinkProps = {
+  name: string;
+  href: string;
+};
+
+export default function MegaMenuLink(props: MegaMenuLinkProps) {
+  const pathname = usePathname();
+
+  return (
+    <NextLink
+      className={twMerge(
+        "flex items-center gap-2 rounded-md px-3 py-2 focus:outline-0",
+        props.href === pathname ? "bg-gray-400" : "hover:bg-gray-400",
+      )}
+      href={props.href}
+    >
+      {props.name}
+    </NextLink>
+  );
+}
--- apps/www/app/[locale]/(pages)/@header/[[...slug]]/mega-menu.tsx
+++ apps/www/app/[locale]/(pages)/@header/[[...slug]]/mega-menu.tsx
@@ -17,48 +17,8 @@ export default function MegaMenu(props: MegaMenuProps) {
         {props.name}
         <IcomoonIcon className="text-xs transition-transform duration-300 group-hover:rotate-90" name="chevron-down" />
       </button>
-      <div className="pointer-events-none absolute left-1/2 top-full flex -translate-x-1/2 gap-8 rounded-md bg-gray-900 px-8 py-4 opacity-0 shadow transition-opacity group-hover:pointer-events-auto group-hover:opacity-100 group-active:pointer-events-auto group-active:opacity-100">
+      <div className="pointer-events-none absolute left-1/2 top-full flex -translate-x-1/2 flex-col gap-2 rounded-md bg-gray-500 p-2 opacity-0 shadow transition-opacity group-hover:pointer-events-auto group-hover:opacity-100 group-active:pointer-events-auto group-active:opacity-100">
         {props.children}
-        {/*<div className="flex gap-8 rounded-md bg-gray-900 px-8 py-4 shadow">*/}
-        {/*  <div className="flex shrink-0 flex-col gap-1">*/}
-        {/*    {sort(filter(groups, "menuAttached"), "order").map((group) => (*/}
-        {/*      <button*/}
-        {/*        key={group.id}*/}
-        {/*        onClick={() => setActiveGroup(group)}*/}
-        {/*        className={`p-3 font-semibold transition-colors ${*/}
-        {/*          activeGroup.id === group.id ? "text-blue-300" : "hover:text-blue-500"*/}
-        {/*        }`}*/}
-        {/*      >*/}
-        {/*        {group.title}*/}
-        {/*      </button>*/}
-        {/*    ))}*/}
-        {/*  </div>*/}
-        {/*  <div className="relative flex w-screen max-w-md items-start gap-5 xl:max-w-2xl">*/}
-        {/*    {"items" in activeGroup &&*/}
-        {/*      activeGroup.items.map((item) => {*/}
-        {/*        if ("related" in item && item.related) {*/}
-        {/*          const href = getHref(item.related.attributes);*/}
-
-        {/*          if (typeof href === "string" && item.related.attributes.publishedAt) {*/}
-        {/*            const url = `/${locale}${href ? "/" + href : ""}`;*/}
-
-        {/*            return (*/}
-        {/*              <NextLink*/}
-        {/*                key={item.id}*/}
-        {/*                href={url}*/}
-        {/*                className="flex w-1/2 gap-2 transition-colors hover:text-blue-500 xl:w-1/3"*/}
-        {/*              >*/}
-        {/*                <span className="flex h-12 w-12 rounded-sm bg-gray-400"></span>*/}
-        {/*                <span className="grow font-semibold">{item.title}</span>*/}
-        {/*              </NextLink>*/}
-        {/*            );*/}
-        {/*          }*/}
-        {/*        }*/}
-
-        {/*        return null;*/}
-        {/*      })}*/}
-        {/*  </div>*/}
-        {/*</div>*/}
       </div>
     </div>
   );
