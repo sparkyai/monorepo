125b48fa1543a0d2173687a08ab352249dcef742
vladyslav <vladstsk@gmail.com>
2023-09-23 16:28:46 +0400

    fix(www): mega menu link view

--- apps/www/app/[locale]/(pages)/@header/[[...slug]]/mega-menu-link.tsx
+++ apps/www/app/[locale]/(pages)/@header/[[...slug]]/mega-menu-link.tsx
@@ -15,7 +15,7 @@ export default function MegaMenuLink(props: MegaMenuLinkProps) {
   return (
     <NextLink
       className={twMerge(
-        "flex items-center gap-2 rounded-md px-3 py-2 focus:outline-0",
+        "flex items-center gap-2 truncate rounded-md px-3 py-2 focus:outline-0",
         props.href === pathname ? "bg-gray-400" : "hover:bg-gray-400",
       )}
       href={props.href}
