02f411e2152eb076f86c1afb56334e625d6cbff6
vladyslav <vladstsk@gmail.com>
2023-10-24 16:28:51 +0400

    fix(tool): show page error message

--- apps/tool/app/(content)/(pages)/error.tsx
+++ apps/tool/app/(content)/(pages)/error.tsx
@@ -1,14 +1,22 @@
 "use client";
 
+import { useEffect } from "react";
+
 type PageErrorProps = {
-  error: Error;
+  error: Error & {
+    digest?: string;
+  };
 };
 
 export default function PageError(props: PageErrorProps) {
+  useEffect(() => {
+    // eslint-disable-next-line no-console -- ignore
+    console.error(props.error.message, props.error.digest);
+  }, [props.error]);
+
   return (
     <div className="flex h-full flex-col items-center justify-center gap-2">
       <h2 className="text-3xl font-bold text-rose-500">Something went wrong!</h2>
-      <p>Error: {props.error.message}</p>
     </div>
   );
 }
