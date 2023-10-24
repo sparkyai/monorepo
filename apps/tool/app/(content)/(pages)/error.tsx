a9566d9d50a1a02f0ce3b190721513e17012cdf1
vladyslav <vladstsk@gmail.com>
2023-10-24 16:24:56 +0400

    fix(tool): show page error message

--- apps/tool/app/(content)/(pages)/error.tsx
+++ apps/tool/app/(content)/(pages)/error.tsx
@@ -1,9 +1,14 @@
 "use client";
 
-export default function Error() {
+type PageErrorProps = {
+  error: Error;
+};
+
+export default function PageError(props: PageErrorProps) {
   return (
-    <div className="flex h-full flex-col gap-2">
-      <h2 className="m-auto text-3xl font-bold text-rose-500">Something went wrong!</h2>
+    <div className="flex h-full flex-col items-center justify-center gap-2">
+      <h2 className="text-3xl font-bold text-rose-500">Something went wrong!</h2>
+      <p>Error: {props.error.message}</p>
     </div>
   );
 }
