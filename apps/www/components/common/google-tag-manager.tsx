9d6a07ae24a55c56743b51c6347b90ec024a0230
vladyslav <vladstsk@gmail.com>
2023-10-29 15:05:40 +0400

    fix(www): modify google tag manager

--- apps/www/components/common/google-tag-manager.tsx
+++ apps/www/components/common/google-tag-manager.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import Script from "next/script";
+import { useSearchParams } from "next/navigation";
 
 type GoogleTagManagerProps = {
   id?: string;
 };
 
 export default function GoogleTagManager(props: GoogleTagManagerProps) {
-  if (process.env.NODE_ENV !== "production" || !props.id) {
-    return null;
+  const searchParams = useSearchParams();
+
+  if (searchParams.has("gtm_debug") || (process.env.NODE_ENV === "production" && props.id)) {
+    return <Script async src={`https://www.googletagmanager.com/gtm.js?id=${props.id}`} />;
   }
 
-  return <Script async src={`https://www.googletagmanager.com/gtm.js?id=${props.id}`} />;
+  return null;
 }
