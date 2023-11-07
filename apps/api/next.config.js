cf4a6101e6c8e3cbf435c0baba179c2627f91108
vladyslav <vladstsk@gmail.com>
2023-11-07 18:59:24 +0400

    feat(api): optimize proxy api

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/v1/:path*",
+        permanent: false,
+        destination: `${process.env.TOOL_URL}/api/v1/:path*`,
+      },
+      {
+        source: "/categories",
+        permanent: false,
+        destination: `${process.env.TOOL_URL}/api/categories`,
+      },
+      {
+        source: "/templates/:path*",
+        permanent: false,
+        destination: `${process.env.TOOL_URL}/api/templates/:path*`,
+      },
+    ];
+  },
+
   reactStrictMode: true,
 };
