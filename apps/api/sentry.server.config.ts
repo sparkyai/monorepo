b72bb736b3e52ee0dd234d30ce53518e06b9f91a
vladyslav <vladstsk@gmail.com>
2023-11-07 19:07:05 +0400

    feat(api): connect sentry

---
+++ apps/api/sentry.server.config.ts
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://2e0c3780faabd6bc4ebe089b8afebb1a@o4505285422415872.ingest.sentry.io/4506185188376576",
+
+  enabled: process.env.NODE_ENV === "production",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
